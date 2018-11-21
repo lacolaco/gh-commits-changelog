ac16e1d5ee464c1e66245aec5654b37b35b12251
Suguru Inatomi <suguru.inatomi@gmail.com>
2018-11-21 15:08:15 +0900

    chore: add custom template test

--- src/main.spec.ts
+++ src/main.spec.ts
@@ -26,5 +26,81 @@ describe("main", () => {
       const parsed = await main(fixture, context);
       expect(parsed).toMatchSnapshot();
     });
+
+    test("custom commit template", async () => {
+      const fixture: PullsListCommitsResponse = require("../test/fixtures/angularPullRequestCommits.json");
+      const parsed = await main(
+        fixture,
+        context,
+        {},
+        {
+          commitPartial: customCommitTemplate
+        }
+      );
+      expect(parsed).toMatchSnapshot();
+    });
   });
 });
+
+const customCommitTemplate = `
+*{{#if scope}} **{{scope}}:**
+{{~/if}} {{#if subject}}
+  {{~subject}}
+{{~else}}
+  {{~header}}
+{{~/if}} (@{{author}})
+
+{{~!-- commit link --}} {{#if @root.linkReferences~}}
+  ([{{hash}}](
+  {{~#if @root.repository}}
+    {{~#if @root.host}}
+      {{~@root.host}}/
+    {{~/if}}
+    {{~#if @root.owner}}
+      {{~@root.owner}}/
+    {{~/if}}
+    {{~@root.repository}}
+  {{~else}}
+    {{~@root.repoUrl}}
+  {{~/if}}/
+  {{~@root.commit}}/{{hash}}))
+{{~else}}
+  {{~hash}}
+{{~/if}}
+
+{{~!-- commit references --}}
+{{~#if references~}}
+  , closes
+  {{~#each references}} {{#if @root.linkReferences~}}
+    [
+    {{~#if this.owner}}
+      {{~this.owner}}/
+    {{~/if}}
+    {{~this.repository}}#{{this.issue}}](
+    {{~#if @root.repository}}
+      {{~#if @root.host}}
+        {{~@root.host}}/
+      {{~/if}}
+      {{~#if this.repository}}
+        {{~#if this.owner}}
+          {{~this.owner}}/
+        {{~/if}}
+        {{~this.repository}}
+      {{~else}}
+        {{~#if @root.owner}}
+          {{~@root.owner}}/
+        {{~/if}}
+          {{~@root.repository}}
+        {{~/if}}
+    {{~else}}
+      {{~@root.repoUrl}}
+    {{~/if}}/
+    {{~@root.issue}}/{{this.issue}})
+  {{~else}}
+    {{~#if this.owner}}
+      {{~this.owner}}/
+    {{~/if}}
+    {{~this.repository}}#{{this.issue}}
+  {{~/if}}{{/each}}
+{{~/if}}
+`;
